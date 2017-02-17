cae1e33a1a44383ec4324c1351ab20fa25eee361
shuwa <mana_burn_vo@yahoo.co.jp>
2017-02-17 11:35:37 +0900

    encode_operator doctest

--- operator_extended.py
+++ operator_extended.py
@@ -1,5 +1,33 @@
 from sas import Operator
 def encode_operator(original_op, primary2secondary, original_secondary_var, eff_var):
+    '''
+    >>> op = Operator('op',0)
+    >>> primary2secondary = {((0,1),):2}
+    >>> original_secondary_var = set()
+    >>> eff_var = {0}
+    >>> op.from_requirement({0:1,1:0},{0:0})
+    begin_operator
+    op
+    1
+    1 0
+    1
+    0 0 1 0
+    0
+    end_operator
+    <BLANKLINE>
+    >>> encode_operator(op, primary2secondary, original_secondary_var, eff_var)
+    begin_operator
+    op
+    2
+    1 0
+    2 1
+    1
+    0 0 -1 0
+    0
+    end_operator
+    <BLANKLINE>
+    '''
+
     op = OperatorExtended(original_op.name, original_op.cost)
 
     primary_req = {(var,value) for var,value in original_op.requirement.items() if not var in original_secondary_var}
